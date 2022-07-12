f48f49fc446eb31d535e0c2865c16d3d8e5c88c9
sebastien-d-fr <contact@sebastien-d.me>
2022-07-12 02:34:27 +0200

    Possibilite de changer l ordre du tableau

--- projet/src/components/DataTable/DataTable.js
+++ projet/src/components/DataTable/DataTable.js
@@ -1,24 +1,52 @@
 import "./DataTable.css";
+import { useState, useEffect } from "react";
 
 
 function DataTable({data, nameData}) {
+    // Sort data
+    const [dataSorted, setDataSorted] = useState(data);
+    const [dataShow, setDataShow] = useState(true);
+
+    const sortData = (order, type) => {
+        /* Do the filter */
+        if(order === "ASC") {
+            setDataSorted(dataSorted.sort((a, b) => a[type].localeCompare(b[type])));
+        } else {
+            setDataSorted(dataSorted.sort((a, b) => b[type].localeCompare(a[type])));
+        }
+
+        /* Refresh data */
+        setDataShow(false);
+    }
+
+    /* Refresh data */
+    useEffect(() => {
+        setDataShow(true)
+    }, [dataShow]);
+
+
+    // Template
     return(
         <table className="data-table">
             {/* Nom des propriétés */}
             <thead className="data-table-head">
                 <tr className="data-table-row">
-                {
-                    nameData.map((name, index) => 
-                        <th className="data-table-head-value" key={index}>{name}</th>
-                    )
-                }
+                    {
+                        nameData.map((name, index) =>
+                            <td className="data-table-sort" key={index}>
+                                <i className="data-table-sort-arrow ri-arrow-down-s-line" onClick={() => sortData("ASC", name)}></i>
+                                {name}
+                                <i className="data-table-sort-arrow ri-arrow-up-s-line" onClick={() => sortData("DESC", name)}></i> 
+                            </td>
+                        )
+                    }
                 </tr>
             </thead>
 
             {/* Valeurs des propriétés */}
             <tbody className="data-table-body">
                 {
-                    data.map((element, index) =>
+                    dataShow === true && dataSorted.map((element, index) => 
                         <tr className="data-table-row" key={index}>
                             {
                                 nameData.map((name, index) => 
