0d2a3166f43c899f7b754bbb5b0db8285a47c6e8
sebastien-d-fr <contact@sebastien-d.me>
2022-10-26 04:12:50 +0200

    Update RoutesPath.js

--- site/src/routes/RoutesPath.js
+++ site/src/routes/RoutesPath.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "../layouts/Layout";
 import Create from "../pages/Create/Create";
 import List from "../pages/List/List";
@@ -7,15 +7,15 @@ import List from "../pages/List/List";
 function RoutesPath() {
     // Template
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             <Layout>
                 <Routes>
-                    <Route element={<Create />} path="/" />
+                    <Route element={<Navigate replace to="/create" />} path="/" />
                     <Route element={<Create />} path="/create" />
                     <Route element={<List />} path="/list" />
                 </Routes>
             </Layout>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
